d6cba41130d2a5e7b30ec8e4244cb7972224e277
MattSeligman <matt@modelitup.com>
2022-04-20 01:19:34 -0700

    Revise to properly get and set values

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,61 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
 import classNames from "classnames";
 
 import "./DayListItem.scss";
 
 export default function DayListItem(props) {
 
+  // Pull entries from Parent and assign to props object.
   let { name, spots, selected } = props;
-  const [ getSpots, setSpots ] = useState(5);
-  const [ getSpotsMessage, setSpotsMessage ] = useState(`${getSpots} spots remaining`);
 
+  // Assign classes with classNames npm
   let dayClass = classNames(
-    // included
+
+    // included classes
     "day-list__item",
 
-    // conditionally included
-  {
-    "day-list__item--selected": selected === true,
-    "day-list__item--full": spots === 0
-  }
+    // conditionally included classes
+    {
+      "day-list__item--selected": selected,
+      "day-list__item--full": spots === 0
+    }
   );
 
-  const selectDay = () => {
+  // Day State on Click
+  const selectDay = ()=>{
     props.setDay(name);
-
-    // assign the setSpot & setSpot Message
-    setSpots( (currentSpot) => { 
-
-      let currentSpotMinusOne = (currentSpot - 1);
-
-      if(currentSpotMinusOne === 1){
-        setSpotsMessage(`${currentSpotMinusOne} spot remaining`);
-        return currentSpotMinusOne;
-      }
-
-      if(currentSpotMinusOne === 0 || currentSpot === 0){
-        setSpotsMessage(`no spots remaining`);
-        return 0;
-      }
-
-      // basecase
-      setSpotsMessage(`${currentSpotMinusOne} spots remaining`);
-      return currentSpot - 1;
-    });
   }
 
+  // Message states based on Spots
+  const spotsMessage = (spots)=>{
+    
+    if (spots === 0 ) {
+      return `no spots remaining`;
+    }
+    else if (spots === 1 ) {
+      return `${spots} spot remaining`;
+    }
+
+    return `${spots} spots remaining`;
+  }
+  
   return (
     <li 
       className={dayClass}
       onClick={selectDay}
-      spots={getSpots}
+      spots={spots}
     >
       <h2 
         className="text--regular"
       >{name}</h2>
       <h3 
-      className="text--light"
-      >{getSpotsMessage}</h3>
+        className="text--light"
+      >{spotsMessage(spots)}</h3>
     </li>
   );
 }

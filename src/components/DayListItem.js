2c2a4392b409a9541e26e564a291e5a3ca7f6ee6
MattSeligman <matt@modelitup.com>
2022-04-19 23:03:24 -0700

    Add DayListItem *to-do: review jest*

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,32 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 
 import "./DayListItem.scss";
 
 export default function DayListItem(props) {
 
-  let { name, spots } = props;
+  let { name, spots, selected } = props;
+  const [ getSpots, setSpots ] = useState(5);
+  const [ getSpotsMessage, setSpotsMessage ] = useState(`${getSpots} spots remaining`);
 
-  let dayClass = classNames("day-list__item",{
-    "day-list__item--selected": props.selected === true,
-    "day-list__item--full": props.spots === 0
-  });
-  
+  let dayClass = classNames(
+    // included
+    "day-list__item",
+
+    // conditionally included
+  {
+    "day-list__item--selected": selected === true,
+    "day-list__item--full": spots === 0
+  }
+  );
+
   const selectDay = () => {
     props.setDay(name);
+
+    // assign the setSpot & setSpot Message
+    setSpots( (currentSpot) => { 
+
+      let currentSpotMinusOne = (currentSpot - 1);
+
+      if(currentSpotMinusOne === 1){
+        setSpotsMessage(`${currentSpotMinusOne} spot remaining`);
+        return currentSpotMinusOne;
+      }
+
+      if(currentSpotMinusOne === 0 || currentSpot === 0){
+        setSpotsMessage(`no spots remaining`);
+        return 0;
+      }
+
+      // basecase
+      setSpotsMessage(`${currentSpotMinusOne} spots remaining`);
+      return currentSpot - 1;
+    });
   }
 
   return (
     <li 
       className={dayClass}
       onClick={selectDay}
+      spots={getSpots}
     >
       <h2 
         className="text--regular"
       >{name}</h2>
       <h3 
       className="text--light"
-      >{spots} spots remaining</h3>
+      >{getSpotsMessage}</h3>
     </li>
   );
 }

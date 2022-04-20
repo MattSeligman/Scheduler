b2135503f76c57e28ffb9dbcffd3ac650881728e
MattSeligman <matt@modelitup.com>
2022-04-20 01:18:22 -0700

    Add DayList with map which adds DayListItems

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,10 +3,22 @@ import DayListItem from "./DayListItem";
 
 export default function DayList(props){
 
+  // pull the days from the props
+  let { days, setDay } = props;
+
   return(
     <ul>
-      <DayListItem />
-
+      {/* loop through the day's entries */}
+      {days.map((day) =>{
+          return <DayListItem 
+            key={day.id}
+            name={day.name} 
+            spots={day.spots} 
+            selected={day.name === props.day}
+            setDay={setDay}  
+          />;
+        })
+      }
     </ul>
   )
 }

d56cd598cbc1b325aedf38963961f86beaec6fe9
MattSeligman <matt@modelitup.com>
2022-04-20 22:44:47 -0700

    Refactored props

--- src/components/appointments/Empty.js
+++ src/components/appointments/Empty.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 const AppointmentEmpty = (props) => {
+
+  const { onAdd } = props;
+
   return (
     <main className="appointment__add">
       <img
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
-        onClick={props.onAdd}
+        onClick={onAdd}
       />
     </main>
   )

87655f0f0e7318057081e4f98f72d5eb6a79539c
MattSeligman <matt@modelitup.com>
2022-04-20 19:52:51 -0700

    Refactor InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -10,21 +10,15 @@ export default function InterviewerListItem(props) {
 
   // Assign classes with classNames npm
   const classSetup = classNames(
-
     // includes base class, adds selected if selected
     `interviewers__item${selected ? "--selected" : ""}`
   );
-
-  // Day State on Click
-  const selectInterviewer = ()=>{
-    props.setInterviewer(id);
-  }
   
   return (
     <li 
       key={id}
       className={classSetup}
-      onClick={selectInterviewer}
+      onClick={props.setInterviewer}
     >
       <img
         className="interviewers__item-image"

405fcfe1a056d595f49cba0c56918cd0353d1049
MattSeligman <matt@modelitup.com>
2022-04-21 01:04:39 -0700

    Add Appointments Form component

---
+++ src/components/appointments/Form.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
+
+
+const AppointmentForm = (props) => {
+  console.log("Props", props)
+
+  const { interviewers, onSave, onCancel} = props;
+
+  const [student, setStudent] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = ()=>{
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = ()=>{
+    reset();
+    onCancel();
+  }
+
+  const save = ()=>{
+    // saving for API
+    onSave(student, interviewer);
+  }
+  
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={e => e.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(e) => setStudent(e.target.value)}
+          />
+        </form>
+        <InterviewerList 
+          interviewers={interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={ cancel }>Cancel</Button>
+          <Button confirm onClick={ save }>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
+
+export default AppointmentForm

6cef6f4c614aea643284888911f9c687af2da607
MattSeligman <matt@modelitup.com>
2022-04-22 18:35:45 -0700

    Refactor Appointments bug seen in application

--- src/components/appointments/index.js
+++ src/components/appointments/index.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import Header from 'components/appointments/Header'
+import Show from 'components/appointments/Show'
+import Empty from 'components/appointments/Empty'
 import "./styles.scss";
 
 const Appointment = (props) => {
-  const { time } = props;
-  return (
-    <article className="appointment">
-      { time && `Appointment at ${time}`}
-      { !time && `No Appointments`}
+  const { 
+      id, time, interview, onEdit, onDelete, // Show
+      onAdd // Empty
+    } = props;
+
+    console.log(props) 
+  return ( 
+  <article className="appointment">
+      <Header id={ id } time={ time } />
+      { 
+        
+        (interview) ? 
+        
+        // if interview exists
+        <Show 
+          student={ interview.student } 
+          interviewer={ interview.interviewer } 
+          onEdit={ onEdit  } 
+          onDelete={ onDelete } 
+        />
+
+        : // else return
+
+        <Empty
+          onAdd= { onAdd }
+        />  
+      }
+      
+      
     </article>
   )
 }

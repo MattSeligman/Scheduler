eaf3f90de094c23fcd874f55556b331bee202810
MattSeligman <matt@modelitup.com>
2022-04-22 18:38:00 -0700

    Add helper functions selectors.js and selectors.test.js

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+export function getAppointmentsForDay(state, day) {
+        
+    const appointmentOnDay = state.days.filter(theDays => theDays.name === day);
+    if ( appointmentOnDay.length === 0 ){ return [] };
+
+    const appointmentsIDs = appointmentOnDay[0].appointments;
+    
+    let appointmentsForDay = [];
+    
+    appointmentsIDs.forEach((entryID)=>{
+        appointmentsForDay.push(state.appointments[entryID])
+    });
+
+    return appointmentsForDay;
+}

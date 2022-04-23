b426186e15e4c95d44b10f510bc5df30510371c3
MattSeligman <matt@modelitup.com>
2022-04-22 18:34:25 -0700

    Add API & Refactor Days & Appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,66 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from '../components/DayList'
+import Appointment from '../components/appointments/index'
 
-import InterviewerListItem from '../components/InterviewerListItem'
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
-
+import axios from 'axios';
 
 const Application = () =>{
 
-  const [day, setDay] = useState("Monday");
+  const setDay = day => setState({ ...state, day })
+  const setDays = days => setState(prev => ({ ...prev, days }));
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const dailyAppointments = [];
+  
+  const apiDays = `http://localhost:8001/api/days/`;
+  const apiAppointments = `http://localhost:8001/api/appointments`;
+  
+  useEffect(() => {
+    Promise.all([
+      axios.get(apiDays),
+      axios.get(apiAppointments),
+      // axios.get('/third_endpoint')
+
+    ]).then((promiseResp) => {
+      let apiDaysResponse = promiseResp[0].data;
+      let apiAppointmentsResponse = promiseResp[1].data;
+      
+      setDays(apiDaysResponse);
+
+      console.log("apiDaysResponse", apiDaysResponse); // second
+      console.log("apiAppointmentsResponse", apiAppointmentsResponse); // second
+    });
+    
+    // axios.get(apiDays)
+    //   .then(response => {
+    //     console.log("Test", response.data, dailyAppointments)
+    //     setDays(response.data);
+    //   })
+    //   .catch((error)=>{
+    //     console.log("Axios - error:", error );
+    //     console.log("Axios - error.response:", error.response );
+    //     console.log("Axios - error.status:", error.status );
+    //   })
+
+  }, []);
 
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-        className="sidebar--centered"
-        src="images/logo.png"
-        alt="Interview Scheduler"
-      />
-      <hr className="sidebar__separator sidebar--centered" />
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
 
-      <DayList
-        days={days}
-        value={day}
-        onChange={setDay}
-      />
+        <DayList
+          days={state.days}
+          value={state.day}
+          setDay={setDay}
+        />
 
-      <nav className="sidebar__menu"></nav>
-      <img
-        className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
-        alt="Lighthouse Labs"
-      />
+        <nav className="sidebar__menu"></nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
+
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {
+          dailyAppointments.map( appointment => {
+            return (
+              <Appointment
+                key={appointment.id}
+                {...appointment}
+              />
+            );
+          })
+        }
+        <Appointment key="last" time="5pm" />
       </section>
+
     </main>
   );
 }

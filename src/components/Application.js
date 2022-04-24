4b2cfc833afe5c0f90e871d3f1560261184201b7
MattSeligman <matt@modelitup.com>
2022-04-23 18:12:03 -0700

    Revise Appointment as instructed by compass, preferred spread operator prior.

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,28 +2,32 @@ import React, { useState, useEffect } from "react";
 import DayList from '../components/DayList'
 import Appointment from '../components/appointments/index'
 import getAppointmentsForDay from '../helpers/selectors'
+import getInterview from "../helpers/selectors";
 
 import "components/Application.scss";
 import axios from 'axios';
 
 const Application = () =>{
-
-  const setDay = day => setState({ ...state, day })
-  
+    
+  // default states
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  // set the Day's state
+  const setDay = day => setState({ ...state, day })
 
+  // setup the API routes
   const api = {
     GET_DAYS: `http://localhost:8001/api/days`,
     GET_APPOINTMENTS: `http://localhost:8001/api/appointments`,
     GET_INTERVIEWERS: `http://localhost:8001/api/interviewers`
   }
   
+  // update the states based on the routes responses
   useEffect(() => {
     Promise.all(
       [
@@ -33,32 +37,32 @@ const Application = () =>{
       ]
     )
     .then((promiseResp) => {
-
-      let apiResponse = {
-        GET_DAYS: promiseResp[0].data,
-        GET_APPOINTMENTS: promiseResp[1].data,
-        GET_INTERVIEWERS: promiseResp[2].data
-      }
-      console.log("apiResponse.GET_DAYS", apiResponse.GET_DAYS);
-      console.log("apiResponse.GET_APPOINTMENTS", apiResponse.GET_APPOINTMENTS);
-      console.log("apiResponse.GET_INTERVIEWERS", apiResponse.GET_INTERVIEWERS);
         
-      // setState( (prev) => ({ ...prev, days: apiResponse.GET_DAYS, appointments: apiResponse.GET_APPOINTMENTS }) );
-      setState(prev => ({ ...prev, days: apiResponse.GET_DAYS, appointments: apiResponse.GET_APPOINTMENTS }));
-    });
+      setState(prev => (
+        { 
+          ...prev, 
+          days: promiseResp[0].data, 
+          appointments: promiseResp[1].data,
+          interviewers: promiseResp[2].data
+        }
+      ));
+    })
     
-    // axios.get(apiDays)
-    //   .then(response => {
-    //     console.log("Test", response.data, dailyAppointments)
-    //     setDays(response.data);
-    //   })
-    //   .catch((error)=>{
-    //     console.log("Axios - error:", error );
-    //     console.log("Axios - error.response:", error.response );
-    //     console.log("Axios - error.status:", error.status );
-    //   })
-
   }, []);
+
+  // grab the appointments for the day
+  const appointments = getAppointmentsForDay(state, state.day);
+  const schedule = appointments.map( appointment => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+        />
+    );
+  });
 
   return (
     <main className="layout">
@@ -85,16 +89,7 @@ const Application = () =>{
       </section>
 
       <section className="schedule">
-        {
-          dailyAppointments.map( appointment => {
-            return (
-              <Appointment
-                key={appointment.id}
-                {...appointment}
-              />
-            );
-          })
-        }
+       { schedule }
         <Appointment key="last" time="5pm" />
       </section>
 

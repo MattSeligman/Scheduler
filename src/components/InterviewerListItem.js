c206756ed15fe33d1c2048691d77f19ab58d8301
MattSeligman <matt@modelitup.com>
2022-04-25 20:38:59 -0700

    Revise Export

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,31 +3,30 @@ import classNames from "classnames";
 
 import "./InterviewerListItem.scss";
 
-export default function InterviewerListItem(props) {
-
-  // Pull entries from Parent and assign to props object.
-  const { id, name, avatar, selected, setInterviewer } = props;
+function InterviewerListItem(props) {
 
   // Assign classes with classNames npm
   const classSetup = classNames(
+
     // includes base class, adds selected if selected
-    `interviewers__item${selected ? "--selected" : ""}`
+    `interviewers__item${props.selected ? "--selected" : ""}`
   );
-  
-  console.log('listItem', props)
+
   return (
     <li 
-      key={id}
+      key={props.id}
       className={classSetup}
-      onClick={setInterviewer}
-      selected={selected}
+      onClick={props.setInterviewer}
+      selected={props.selected}
     >
       <img
         className="interviewers__item-image"
-        src={avatar}
-        alt={name}
+        src={props.avatar}
+        alt={props.name}
       />
-      {selected ? `${name}` : ""}
+      {props.selected ? `${props.name}` : ""}
     </li>
   );
 }
+
+export default InterviewerListItem;

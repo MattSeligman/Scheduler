7455ed51179df99adabc1cdb20905236d872504d
MattSeligman <matt@modelitup.com>
2022-04-27 19:18:29 -0700

    Add Cypress.io - End to End Testing Framework

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,66 @@
+describe("Appointments", () => {
+
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+
+    cy.get("[data-testid=student-name-input]")
+      .type("Matt Seligman");
+
+    cy.get("[alt='Sylvia Palmer']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Matt Seligman");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+  
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Matt Seligman");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+  
+    cy.contains("Save")
+      .click();
+  
+    cy.contains(".appointment__card--show", "Matt Seligman");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .click({ force: true });
+  
+    cy.contains("Confirm")
+      .click();
+  
+    cy.contains("Deleting")
+      .should("exist");
+
+    cy.contains("Deleting")
+      .should("not.exist");
+  
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+  });
+
+});
+
+
---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,17 @@
+describe("Navigation", () => {
+    it("should visit root", () => {
+        cy.visit("/");
+    });
+
+    it("should navigate to Tuesday", () => {
+        cy.visit("/");
+        
+        cy.contains("section.sidebar > ul > li:nth-child(2)", "Tuesday")
+        .click()
+        .should("have.class", "day-list__item--selected")
+    });
+
+    
+});
+
+
